93aa3755a8f7798ae8e1dcffb207c3ad27e46354
Germangalia <germangalia@iesebre.com>
2016-02-15 18:29:44 +0100

    Create a Stripe form.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,3 +30,9 @@ Route::group(['middleware' => ['web']], function () {
     //
 });
 
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
